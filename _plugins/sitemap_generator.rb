be6232ceaefe621a060cec17ce83d1b0ba145ede
toin0u <acdkfr@gmail.com>
2012-05-14 04:55:01 +0200

    Updated: exclude more page from the sitemap generator

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -47,7 +47,7 @@ module Jekyll
   SITEMAP_FILE_NAME = "sitemap.xml"
 
   # Any files to exclude from being included in the sitemap.xml
-  EXCLUDED_FILES = ["atom.xml", "rss.xml"]
+  EXCLUDED_FILES = ["atom.xml", "rss.xml", "crossdomain.xml", "humans.txt", "robots.txt", "404.html"]
 
   # Any files that include posts, so that when a new post is added, the last
   # modified date of these pages should take that into account
