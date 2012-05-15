9d3e3b617bb5eade1c5583b6d6429ef79f451b7a
toin0u <acdkfr@gmail.com>
2012-05-15 15:51:48 +0200

    Added: last tweet, head.js and cleaned up _includes folder

---
+++ static/js/loader.js
@@ -0,0 +1,24 @@
+/*  
+    Load scripts in parallel but execute in order 
+    @link http://headjs.com
+*/
+
+head.js(
+    { plugins: "/static/js/plugins.js" },
+    { twitter: "/static/js/twitter.js" },
+    function() {
+        head.js(
+            { feed: "http://twitter.com/statuses/user_timeline/toin0u.json?callback=lastTweet&count=5" },
+            function() {
+
+            }
+        );
+    }
+);
+
+//head.js("//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js");
+//window.jQuery || head.js("/static/js/libs/jquery-1.7.1.min.js");
+
+head.ready(function() {
+    //
+});
